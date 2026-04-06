c5aead4d2e8ee292bdf4ceb6cb12cadc069bfd46
Daeki <kimdaeki@gmail.com>
2026-04-06 17:19:29 +0900

    mybatis1

---
+++ src/main/java/com/winter/app/departments/DepartmentDTO.java
@@ -0,0 +1,17 @@
+package com.winter.app.departments;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class DepartmentDTO {
+	private String departmentNo;
+	private String departmentName;
+	private String category;
+	private String openYn;
+	private Long capacity;
+
+}
