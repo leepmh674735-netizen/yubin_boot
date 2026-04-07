7b16a4dbc57327dd69ba4872f13bfe8b482677d8
Daeki <kimdaeki@gmail.com>
2026-04-07 10:56:21 +0900

    mybatis2

--- src/main/java/com/winter/app/departments/DepartmentController.java
+++ src/main/java/com/winter/app/departments/DepartmentController.java
@@ -30,5 +30,39 @@ public class DepartmentController {
 		DepartmentDTO departmentDTO = departmentService.detail(num);
 		System.out.println(departmentDTO);
 	}
+	
+	@GetMapping("create")
+	public void create() {
+		DepartmentDTO departmentDTO = new DepartmentDTO();
+		departmentDTO.setDepartmentNo("070");
+		departmentDTO.setDepartmentName("사학과");
+		departmentDTO.setCategory("인문사회");
+		departmentDTO.setOpenYn("N");
+		departmentDTO.setCapacity(20L);
+		int result = departmentService.create(departmentDTO);
+		System.out.println("희망 값 : 1 , 실제값 :"+ result);
+	}
+	
+	@GetMapping("delete")
+	public void delete(DepartmentDTO departmentDTO) {
+		
+		int result = departmentService.delete(departmentDTO);
+		System.out.println("희망 값 : 1 , 실제값 :"+ result);
+		
+	}
+	
+	@GetMapping("update")
+	public void update() {
+		DepartmentDTO departmentDTO = new DepartmentDTO();
+		departmentDTO.setDepartmentNo("070");
+		departmentDTO.setDepartmentName("고고학");
+		departmentDTO.setCategory("예체능");
+		departmentDTO.setOpenYn("Y");
+		departmentDTO.setCapacity(40L);
+		int result = departmentService.update(departmentDTO);
+		
+		System.out.println("희망 값 : 1 , 실제값 :"+ result);
+		
+	}
 
 }
